4bbe7fb218f4606f9da071349d9d44ff06471569
Moises Pinedo <moisespinedo@MacBook-Air-de-Moises.local>
2025-06-30 21:44:38 -0600

    Arregle el bug de Retrofit

--- app/src/main/java/superheroapp/ApiService.kt
+++ app/src/main/java/superheroapp/ApiService.kt
@@ -1,11 +1,11 @@
 package superheroapp
 
-import okhttp3.Response
+import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("api/c9fb57f7ea7b5081e0d3e1153318b830/search/{name}")
-    suspend fun getSuperheroes(@Part("name")superheroName:String):Response<SuperHeroDataResponse>
+    suspend fun getSuperheroes(@Path("name")superheroName:String):Response<SuperHeroDataResponse>
 }
